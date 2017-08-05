f34bff28c4323c7cdabe1f2c8758c8f5753395a1
Jeremy523 <mrjeremydelacruz@gmail.com>
2017-08-05 16:20:49 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = 3000;
+// heroku will set an env PORT value
+const port = process.env.PORT || 3000;
 
 var app = express();
 
